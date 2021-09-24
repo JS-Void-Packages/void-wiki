d11aa62f10ccc9cdbfabca39b1dc9feeb274792a
Mrthomas20121 <thomas77300@gmail.com>
2021-09-24 15:22:31 +0200

    Create resource_location.js

---
+++ library/resource_location.js
@@ -0,0 +1,54 @@
+/*
+    ResourceLocation - a port of Mojang ResourceLocation to javascript
+    Mrthomas20121 - 2021
+*/
+const path = require('path')
+
+class ResourceLocation {
+    /**
+     * 
+     * @param {string} name 
+     * @param {string} resource 
+     */
+    constructor(name, resource) {
+        this.resource_name = name
+        this.path_to_resource = resource
+    }
+
+    /**
+     * create a resourceLocation from an array
+     * @param {string[]} array 
+     */
+    static fromArray(array) {
+        if(array.length > 2) {
+            console.log('Error, Array Length is higher than 2')
+        }
+        else {
+            return new ResourceLocation(array[0], array[1])
+        }
+    }
+
+    /**
+     * Create a resourceLocation from a string
+     * @param {string} str 
+     */
+    static fromString(str) {
+        return this.fromArray(str.split(':'))
+    }
+
+    getPath() {
+        return this.path_to_resource
+    }
+
+    getName() {
+        return this.resource_name
+    }
+
+    getFullPath() {
+        let left_path = ''
+        if(this.resource_name == 'images') {
+            left_path=path.join('.', this.resource_name)
+        }
+        return path.join(left_path, this.path_to_resource)
+    }
+}
