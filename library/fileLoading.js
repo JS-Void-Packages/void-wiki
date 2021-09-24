007610515cf1743d5a8ebcec9a80b8829e5e13a3
Mrthomas20121 <thomas77300@gmail.com>
2021-09-24 14:48:59 +0200

    Create fileLoading.js

---
+++ library/fileLoading.js
@@ -0,0 +1,52 @@
+/*
+    fileLoading.js | Mrthomas20121 - 2021
+    Load the toml files
+*/
+const fs = require('fs')
+const path = require('path')
+const voidLogger = require('void-logger')
+const TOML = require('@iarna/toml')
+const Logger = new voidLogger('void-wiki').setLogLocation('.')
+
+/**
+ * Load a file
+ * @param {string} file 
+ * @param {boolean} shouldLog 
+ * @returns {string}
+ */
+const loadFile = (file, shouldLog=false) => {
+    if(shouldLog) Logger.info(`Loaded file ${file}`)
+    return fs.readFileSync(file, 'utf8')
+}
+
+/**
+ * Load a TOML file
+ * @param {string} file 
+ * @returns {{}}
+ */
+const loadTOML = (file) => {
+    return TOML.parse(loadFile(file))
+}
+
+let templates = {}
+
+const loadTemplates = () => {
+    let f = './docs/template'
+    const folder = fs.readdirSync(f)
+    for(let file of folder) {
+        // only load .toml files
+        if(file.endsWith('.toml')) {
+            let file_path = path.join(f, file)
+            let name = file.split('.')[0]
+            templates[name] = loadTOML(file_path)
+        }
+    }
+}
+
+const readTemplate = () => {
+
+}
+
+loadTemplates()
+
+console.log(templates)
